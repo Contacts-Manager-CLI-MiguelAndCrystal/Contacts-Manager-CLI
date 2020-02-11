142be7f3199ddb267819a4f8cb0acb2eaf59338e
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-02-11 12:30:06 -0600

    added addd requirement

--- src/ContactsApplication.java
+++ src/ContactsApplication.java
@@ -6,12 +6,14 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import util.Input;
 
 
 public class ContactsApplication {
-    static private List<Object> contactsList;
+    static private List<Contact> contactsList;
+    private static Scanner scanner = new Scanner(System.in);
 
     static void newFile() {
         String directory = "data";
@@ -37,94 +39,107 @@ public class ContactsApplication {
             ioe.printStackTrace();
         }
     }
-    static void writeFile(Object person) {
-        contactsList = new ArrayList<>();
-        contactsList.add(person);
-        for (Object contact : contactsList){
-            System.out.println(contact);
-        }
+    static void writeFile(Contact person) {
+//        contactsList = new ArrayList<>();
+//        contactsList.add(person);
+//        for (Object contact : contactsList){
+//            System.out.println(contact);
+//        }
         try {
             Path contactsPATH = Paths.get("data", "contacts.txt");
-            Files.write(contactsPATH, Arrays.asList(convertString(contactsList)), StandardOpenOption.APPEND);
-
+            List<String> newPerson = Arrays.asList(person.getName() + " " + person.getNumber());
+            Files.write(contactsPATH, newPerson, StandardOpenOption.APPEND);
+//
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
 
-    public static String[] convertString(List<Object> item){
-        String[] array = new String[item.size()];
-        int index = 0;
-        for (Object value : item) {
-            array[index] = (String) value;
-            index++;
-        }
-        return array;
-    }
-
-//    static void readWriteFile(){
-//        List<String> contactsList = null;
-//
-//        try{
-//            Path contact = Paths.get("data", "contacts.txt");
-//            contactsList = Files.readAllLines(contact);
-//            for(String line : contactsList){
-//                System.out.println(line);
-//            }
-//        }catch(IOException ioe){
-//            ioe.printStackTrace();
-//        }
-//
-//        contactsList.add("kiwi"); contactsList.add("jackfruit");
-//
-//        try{
-//            Path contact = Paths.get("data", "contact.txt");
-//            Files.write(contact, contactsList);
-//        }catch(IOException ioe){
-//            ioe.printStackTrace();
-//        }
+//    public static String[] convertString(Contact person){
+//        String[] array = new String[1];
+//        return array;
 //    }
+
+    static void readWriteFile(){
+        List<String> contactsList = null;
+
+        try{
+            Path contact = Paths.get("data", "contacts.txt");
+            contactsList = Files.readAllLines(contact);
+            for(String line : contactsList){
+                System.out.println(line);
+            }
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        try{
+            Path contact = Paths.get("data", "contact.txt");
+            Files.write(contact, contactsList);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         newFile();
-//        readWriteFile();
+//        Contact person1 = new Contact("Miguel", "775-313-5602");
+//        Contact person2 = new Contact("Crystal", "676-678-7854");
+//        writeFile(person1.getName() + " " + person1.getNumber());
+//        writeFile(person2.getName() + " " + person2.getNumber());
+        getContacts();
+    }
 
+    public static void getContacts(){
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
         boolean decision = true;
         Input input = new Input();
 
 
         while(decision) {
-            System.out.println("1. View contacts.");
-            System.out.println("2. Add a new contact.");
-            System.out.println("3. Search a contact by name.");
-            System.out.println("4. Delete an existing contact.");
-            System.out.println("5. Exit.");
 
-
-//            Contact person1 = new Contact("Miguel", "775-313-5602");
-//            Contact person2 = new Contact("Crystal", "676-678-7854");
-
-//            writeFile(person1.getName() + " " + person1.getNumber());
-//            writeFile(person2.getName() + " " + person2.getNumber());
 
             int choice = input.getInt(1,5);
 
-
             switch (choice) {
                 case(1):
+                    readWriteFile();
+                    getContacts();
+                    break;
+                case(2):
+                    System.out.println("Enter a name");
+                    String userInputName = scanner.nextLine();
+                    System.out.println("Enter a number");
+                    String userInputNumber = scanner.nextLine();
+                    Contact person = new Contact(userInputName, userInputNumber);
+                    writeFile(person);
+                    getContacts();
+                    break;
+                case(3):
+                    System.out.println("Search for a contact by name");
+                    getContacts();
+                    break;
+                case(4):
+                    System.out.println("Delete a contact by name");
+                    getContacts();
+                    break;
+                case(5):
                     boolean confirm = input.yesNo("Are you sure you want to exit?");
                     if(confirm){
                         System.out.println("Goodbye!!!");
                         decision = false;
+                        break;
                     }else{
-                        System.out.println("Restarting......");
+                        System.out.println("Restarting....");
+                        break;
                     }
-                    break;
-                case(2):
             }
 
         }
-
     }
 
 }
